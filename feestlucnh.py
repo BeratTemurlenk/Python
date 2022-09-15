39e0c9cc01a5f85cbcb93dd26ded2808579eba57
berat <9074051@mydavinci.nl>
2022-09-15 13:38:58 +0200

    'eerste'

---
+++ feestlucnh.py
@@ -0,0 +1,19 @@
+
+croissantjes = 0.39
+stokbrood = 2.78
+kortingsbon = 0.50
+
+
+aantal_croissantjes = 17
+aantal_stokbroden = 2
+aantal_kortingsbonnen = 3
+
+print(aantal_croissantjes * croissantjes)
+print(aantal_stokbroden * stokbrood)
+print(aantal_kortingsbonnen * kortingsbon)
+
+print(aantal_croissantjes * croissantjes + aantal_stokbroden * stokbrood - aantal_kortingsbonnen * kortingsbon)
+
+
+
+
