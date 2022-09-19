5ded519aed1c34421638c12890b82780fb2f4ed1
berat <9074051@mydavinci.nl>
2022-09-19 14:47:03 +0200

    'feestlunch.py'

--- feestlunch.py
+++ feestlunch.py
@@ -1,7 +1,5 @@
 aantal_croissantjes = int(input('hoeveel croissantjes wil je?'))
-
-aantal_stokbroden = int(input('hoeveel croissantjes wil je?'))
-
+aantal_stokbroden = int(input('hoeveel stokbroden wil je?'))
 aantal_kortingbonnen = int(input('hoeveel kortingsbonnen heb je?'))
 
 croissantjes = 0.39
@@ -14,10 +12,7 @@ volledig_korting = aantal_kortingbonnen * kortingsbon
 
 totaal_prijs = prijs_croissants + prijs_stokbrood - volledig_korting
 
-
-
-
-
+totale_prijs = round(totaal_prijs, 2)
 
 
 print(f'De feestlunch kost je bij de bakker {totaal_prijs} euro voor de {aantal_croissantjes} croissantjes en de {aantal_stokbroden} stokbroden als de {aantal_kortingbonnen} kortingsbonnen nog geldig zijn!')
