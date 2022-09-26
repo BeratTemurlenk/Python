7703900c40ded4c20de48a27071bf1921df6e878
berat <9074051@mydavinci.nl>
2022-09-26 09:46:01 +0200

    'elif-statement'

--- meten_is_weten.py
+++ meten_is_weten.py
@@ -1,7 +1,12 @@
 A = int(input('Vul een geheel getal in!'))
 B = int(input('Vul nog een geheel getal in!'))
 
-max = A
+
 
 if A > B:
+    max = A
     print(f'A is het grootste getal, De waarde van A is {max}!') 
+
+elif A < B:
+    min = A
+    print(f'A is het kleinste getal, De waarde van A is {min}  ')
