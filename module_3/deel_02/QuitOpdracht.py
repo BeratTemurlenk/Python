1cdb02d182258aae2e087655f308e5bbeb1d6577
berat <9074051@mydavinci.nl>
2022-11-14 14:04:54 +0100

    optelsom1000.py

--- module_3/deel_02/QuitOpdracht.py
+++ module_3/deel_02/QuitOpdracht.py
@@ -1,14 +1,20 @@
-x = True
-teller = 0
-while x :
-    vraag = input('?')
-    teller += 1
-    if vraag == 'quit':
-        x = False
+# x = True
+# teller = 0
+# while x :
+#     vraag = input('?')
+#     teller += 1
+#     if vraag == 'quit':
+#         x = False
 
-print(teller)
+# print(teller)
 
-    
+
+teller2 = 0
+
+while input("?") != "quit":
+    teller2 += 1
+
+print(teller2)
 
        
     
