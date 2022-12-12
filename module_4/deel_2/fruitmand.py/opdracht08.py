0e42eb52edf454219fad27ad559f9c7b45bdfc85
berat <9074051@mydavinci.nl>
2022-12-12 14:43:08 +0100

    opdracht08.py

--- module_4/deel_2/fruitmand.py/opdracht08.py
+++ module_4/deel_2/fruitmand.py/opdracht08.py
@@ -2,5 +2,15 @@ from fruitmand import fruitmand
 
 fruitmand.append({'name': 'watermeloen', 'weight': 900, 'color': 'green', 'round': True})
 
+
+totaal = 0
+
 for x in fruitmand:
-    print(x['weight'])
+   totaal = totaal + x['weight'] 
+
+print(totaal)
+    
+
+
+
+     
