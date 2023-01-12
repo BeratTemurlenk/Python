e2f95a710af5dbe7487f1465f6c26f106a121545
berat <9074051@mydavinci.nl>
2023-01-12 10:07:43 +0100

    fruitmand

---
+++ module_4/deel_2/fruitmand.py/opdracht10.py
@@ -0,0 +1,18 @@
+from fruitmand import fruitmand
+
+
+gewicht_lijst = []    
+fruit_lijst = []
+
+for x in fruitmand:
+    gewicht = x['weight'] / 1000
+    fruit_naam = x['name']
+    fruit_lijst.append(fruit_naam)
+    gewicht_lijst.append(gewicht)
+    gewicht_lijst.sort(reverse=True)
+
+print(gewicht_lijst)
+print(fruit_lijst)
+
+
+    
