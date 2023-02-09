2f9ac66686feea16f3d01942ab26dcd4cae6f7f9
berat <9074051@mydavinci.nl>
2023-02-09 15:08:28 +0100

    M5_deel1

--- module_5/deel_1/Fibonacci2.py
+++ module_5/deel_1/Fibonacci2.py
@@ -2,18 +2,24 @@
 def fibo_lijst(fibo_lijst1, vraag,) -> int:
 
     
-    for x in range(vraag_getal - 2):
-        getal_1 = fibo_lijst1[-1]
-        getal_2 = fibo_lijst1[-2]
-        antwoord = getal_1 + getal_2
-        fibo_lijst1.append(antwoord)
+    
+    getal_1 = fibo_lijst1[-1]
+    getal_2 = fibo_lijst1[-2]
+    antwoord = getal_1 + getal_2
+    fibo_lijst1.append(antwoord)
+    
+    print(fibo_lijst1)
+    
+    
+    if len(fibo_lijst1) <= vraag_getal:
+        return(fibo_lijst(fibo_lijst1, vraag))
+    return(fibo_lijst1)
 
-    if len(fibo_lijst1) >= vraag_getal:
-        gulden_snede = antwoord / getal_2
-        return gulden_snede
 
 start = [0, 1]
 vraag_getal = int(input('geef een getal!'))
+ 
+
 
 gulden_snede = fibo_lijst(start, vraag_getal,)
 print(gulden_snede)
