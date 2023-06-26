b51bff48f553924e25c676acdfcf722c49c60ec7
berat <9074051@mydavinci.nl>
2023-06-26 11:11:14 +0200

    py

--- m5/proef_a/functions_papi.py
+++ m5/proef_a/functions_papi.py
@@ -1,12 +1,19 @@
+PRIJS_IJSBOL = 1.10
+PRIJS_HORRENTJE = 1.25
+PRIJS_BAKJE = 0.75
+
+
 
 
 
 #stap 1:
 
+
+
 def get_aantal_bolletjes():
     while True:
         try:
-            aantal_bolletjes = int(input('Hoeveel bolletjes wilt u?'))
+            aantal_bolletjes = int(input('Hoeveel bolletjes wilt u?'))    
             if aantal_bolletjes >= 1 and aantal_bolletjes <= 3:
                 break
             elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
@@ -19,27 +26,50 @@ def get_aantal_bolletjes():
     return aantal_bolletjes
 
 
+lijst_ijsjes = []
 
+def totaal_aantal_bollen(aantal_bolletjes):
+    lijst_ijsjes.append(aantal_bolletjes)
+    totaal = sum(lijst_ijsjes)
+    return totaal
+
+
+
+
+
+dict_bak_hoorn = {'bakje': 0,
+                  'hoorntje': 0}
 
 #stap 2
 def get_hoorntje_bakje(aantal_bolletjes):
     if aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
         print(f'Hier is uw bakje met {aantal_bolletjes} bolletje(s).')
+        dict_bak_hoorn['bakje'] += 1
         return 'bakje'
     while True:
         vraag_hoorntje_bakje = input(f'Wilt u deze {aantal_bolletjes} bolletje(s) in een hoorntje of een bakje?')
-        if vraag_hoorntje_bakje == 'hoorntje' or vraag_hoorntje_bakje == 'bakje':
+        if vraag_hoorntje_bakje == 'hoorntje':
             print(f'Hier is uw {vraag_hoorntje_bakje} met {aantal_bolletjes} bolletje(s).')
+            dict_bak_hoorn['hoorntje'] += 1
             break
+        elif vraag_hoorntje_bakje == 'bakje':
+            print(f'Hier is uw {vraag_hoorntje_bakje} met {aantal_bolletjes} bolletje(s).')
+            dict_bak_hoorn['bakje'] += 1
+            break
+        
         else:
             print('Sorry, dat snap ik niet..')
     return vraag_hoorntje_bakje
-    
+
+   
+
+
+
 
 #stap 3
 def Meer_bestellen_ofniet():
-    vraag_meer_bestellen = input('Wilt u nog meer bestellen?')
     while True:
+        vraag_meer_bestellen = input('Wilt u nog meer bestellen?')
         if vraag_meer_bestellen == 'ja':
             break
         elif vraag_meer_bestellen == 'nee':
@@ -49,3 +79,13 @@ def Meer_bestellen_ofniet():
             print('Sorry, dat snap ik niet..')
     return vraag_meer_bestellen
 
+
+
+
+
+
+
+    
+    
+    
+    
--- m5/proef_a/papi_gelato.py
+++ m5/proef_a/papi_gelato.py
@@ -1,12 +1,40 @@
 from functions_papi import *
-  
+import math
 
 print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
+
+
 meer_bestellen = 'ja'
 while meer_bestellen == 'ja':
     aantal_bolletjes = get_aantal_bolletjes()
+    totaal_bollen = totaal_aantal_bollen(aantal_bolletjes)
     hoorntje_bakje = get_hoorntje_bakje(aantal_bolletjes)
     meer_bestellen = Meer_bestellen_ofniet()
 
+
+
+
+prijs_alle_bakjes = dict_bak_hoorn['bakje'] * PRIJS_BAKJE
+prijs_alle_hoorntjes = dict_bak_hoorn['hoorntje'] * PRIJS_HORRENTJE
+prijs_alle_bollen = totaal_bollen * PRIJS_IJSBOL
+
+
+
+bonnetje = ''
+bonnetje += (f' --------------------["Papi Gelato"]--------------------\n')
+bonnetje += (   f'Bolletjes {totaal_bollen} x {PRIJS_IJSBOL} = $ {prijs_alle_bollen:.2f}\n')
+if dict_bak_hoorn["hoorntje"] >= 1:      
+    bonnetje += (   f'Hoorntje  {dict_bak_hoorn["hoorntje"]} x {PRIJS_HORRENTJE} = $ {prijs_alle_hoorntjes:.2f}\n')
+if dict_bak_hoorn["bakje"] >= 1:      
+    bonnetje += (   f'Bakjes {dict_bak_hoorn["bakje"]} x {PRIJS_BAKJE} = $ {prijs_alle_bakjes:.2f}\n')  
+bonnetje += ('-------------------------------------------------------- +\n')     
+Totaal = prijs_alle_bakjes + prijs_alle_bollen + prijs_alle_hoorntjes
+bonnetje += (   f'Totaal                = $ {Totaal:.2f}\n')
+bonnetje += ('Bedankt en tot ziens!\n')
+
+
+            
+print(bonnetje)
+    
 
 
